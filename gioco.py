4fba615df94c32cc87e9ae1d9ab190f00f558871
Lava Matteo <lava.matteo.06@itisdalmine.edu.it>
2023-05-21 13:59:59 +0200

    creato il gioco base

---
+++ gioco.py
@@ -0,0 +1,10 @@
+from ursina import * 
+from ursina.prefabs.first_person_controller import *
+from ursina.shaders import lit_with_shadows_shader, transition_shader
+
+app = Ursina()
+
+
+
+
+app.run()
